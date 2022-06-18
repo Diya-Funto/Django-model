8fb2c078dba857fd5c2c0945ce887823dfe19e61
Funto Sofidiya <funto.s@yahoo.com>
2022-06-18 18:58:44 +0100

    more changes

--- I4G002064CAE/blog/admin.py
+++ I4G002064CAE/blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post
+
+class PostDB(admin.ModelAdmin):
+    list_display = [
+        "Title", "Text", "Author", "Created_date", "Published_date"
+]
+
 
 # Register your models here.
+admin.site.register(Post, PostDB)
